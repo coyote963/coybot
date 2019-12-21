dba8b68457a9a05c66a5f79a40e331eb70321208
John Luan <yxl1079@case.edu>
2019-12-21 01:03:56 -0500

    added search to the discord bot

---
+++ cogs/search_cog.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+from cogs.search_function import get_player, get_steam_profile, get_hex_code, get_hex
+from cogs.secrets import uri
+from pymongo import MongoClient
+from cogs.game_ids import hats, premium, store
+import discord
+import asyncio
+
+class Search(commands.Cog):
+    """This part of the bot is for returning search"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = MongoClient(uri).bmdb
+
+    @commands.command()
+    async def profile(self, ctx, query: str = ""):
+        if len(query) != 0:
+            player = get_player(self.db, query)
+            if player is None:
+                await ctx.send("Please check your spelling")
+            else:
+                alternate_names = []
+                for i in range(1, len(player['name'])):
+                    alternate_names.append(player['name'][i])
+                embed = discord.Embed(
+                    title = player['name'][0],
+                    description = "aliases : " + str(alternate_names),
+                    colour = get_hex_code(get_hex(int(player['color']))),
+                    url = "https://steamcommunity.com/profiles/{}".format(player['_id']['profile'])
+                )
+                
+                profile = get_steam_profile(player)
+                if 'premium' in player:
+                    embed.add_field(name="status", value = premium[int(player['premium'])])
+                embed.add_field(name="hat", value=hats[int(player['hat'])])
+                embed.add_field(name="platform", value = store[int(player['_id']['platform'])])
+                embed.set_image(url =  profile['response']['players'][0]['avatarfull'])
+                await ctx.send(embed = embed)
+        else:
+            await ctx.send("Provide a username")
+
+
+def setup(bot):
+    bot.add_cog(Search(bot))
---
+++ cogs/search_function.py
@@ -0,0 +1,29 @@
+import requests
+import re
+from cogs.secrets import steam_key
+def get_hex(colorvalue):
+    b = int(colorvalue / (256 ** 2))
+    remainder = colorvalue % (256 ** 2)
+    g = int(remainder / 256)
+    r = remainder % 256
+    return (r,g,b)
+
+def get_hex_code(color_tuple):
+    return color_tuple[0] << 16 | color_tuple[1] << 8 | color_tuple[2]
+
+def get_color_name(r,g,b):
+    response = requests.get("https://www.thecolorapi.com/id?rgb=rgb({},{},{})".format(r, g, b))
+    print ("{} {} {}".format(r,g,b))
+    return response.json()['name']['value']
+
+def get_player(db, query):
+    player = db.players.find_one(
+        {'name' : {'$regex' : re.compile(re.escape(query), re.IGNORECASE)}})
+    if player is not None:
+        return player
+
+def get_steam_profile(player):
+    if player['_id']['platform'] == '0':
+        steam_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}".format(steam_key,player['_id']['profile'])
+        response = requests.get(steam_url)
+        return response.json()
